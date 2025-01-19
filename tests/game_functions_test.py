25e25dd41bd6bc87e97f231460ce07aa5b684465
rrahn <rene.rahn@fu-berlin.de>
2025-01-19 15:14:33 +0100

    Add initial test for computing number of rows for alien fleet

---
+++ tests/game_functions_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from dataclasses import dataclass
+from src.game_functions import get_number_rows
+
+@dataclass
+class TestSettings():
+    screen_height = 600
+
+def test_get_number_rows():
+    assert get_number_rows(TestSettings(), 80, 50) == 3
+    assert get_number_rows(TestSettings(), 20, 20) == 4
